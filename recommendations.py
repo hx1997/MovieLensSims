109fecaa21ad7d050b2df69af1ec3e00a6081bfe
hx1997 <hx970923@gmail.com>
2017-04-26 23:42:39 +0800

    MovieLensSims: Implement findNearestNeighbor()

--- recommendations.py
+++ recommendations.py
@@ -14,15 +14,27 @@ def loadMovieLens(path='./datasets/ml-latest-small'):
 
 	return prefs
 
-def simsDistance(user1, user2):
+def simDistance(prefs, user1, user2):
 	distance = 0
-	for item in user1:
-		if item in user2:
-			distance += pow(user1[item] - user2[item], 2)
+	for item in prefs[user1]:
+		if item in prefs[user2]:
+			distance += pow(prefs[user1][item] - prefs[user2][item], 2)
 
-	distance = 1 / (1 + math.sqrt(distance))
+	if distance == 0: return 0
 
-	return distance
+	correlation = 1 / (1 + math.sqrt(distance))
 
-def findNearestNeighbor(user):
-	pass
+	return correlation
+
+def findNearestNeighbor(prefs, user, corFunc = simDistance):
+	max = 0
+	maxUser = {}
+
+	for userid in prefs:
+		if prefs[userid] != user:
+			correlation = corFunc(prefs, userid, user)
+			if correlation > max:
+				max = correlation
+				maxUser = userid
+
+	return (max, maxUser)
