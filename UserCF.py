ad40484160fd2e2aaeea833aa8b4b42886cf74c0
hx1997 <hx970923@gmail.com>
2017-04-28 09:17:50 +0800

    Implement Pearson coefficient correlation

--- UserCF.py
+++ UserCF.py
@@ -26,12 +26,45 @@ def simDistance(prefs, user1, user2):
 
 	return correlation
 
-def findNearestNeighbor(prefs, user, corFunc = simDistance):
+def simPearson(prefs, user1, user2):
+	user1Mean = 0
+	user2Mean = 0
+
+	if len(prefs[user1]) * len(prefs[user2]) == 0:
+		return 0
+
+	for item in prefs[user1]:
+		user1Mean += prefs[user1][item]
+	user1Mean /= len(prefs[user1])
+
+	for item in prefs[user2]:
+		user2Mean += prefs[user2][item]
+	user2Mean /= len(prefs[user2])
+
+	num = 0
+	deno1 = 0
+	deno2 = 0
+	for item in prefs[user1]:
+		if item in prefs[user2]:
+			print item
+			num += (prefs[user1][item] - user1Mean) * (prefs[user2][item] - user2Mean)
+			deno1 += pow(prefs[user1][item] - user1Mean, 2)
+			deno2 += pow(prefs[user2][item] - user2Mean, 2)
+
+	deno = math.sqrt(deno1 * deno2)
+
+	if deno == 0: return 0
+
+	correlation = num / deno
+
+	return correlation
+
+def findNearestNeighbor(prefs, user, corFunc = simPearson):
 	max = 0
 	maxUser = {}
 
 	for userid in prefs:
-		if prefs[userid] != user:
+		if userid != user:
 			correlation = corFunc(prefs, userid, user)
 			if correlation > max:
 				max = correlation
